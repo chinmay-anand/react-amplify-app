e0c5662ea94ded16ed0efcfe95d6cffc7f99cded
chinmay-anand <chinmay.subscriptions@gmail.com>
2021-03-23 02:46:07 +0530

    Added a line to App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Chinmay has deployed this app on AWS using AWS Amplify service</p>
         <a
           className="App-link"
           href="https://reactjs.org"
